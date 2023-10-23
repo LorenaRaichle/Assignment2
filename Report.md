a890c2ec3685717ca4c3309619a5cd5881540363
Joey Lau <joeylau.2020@scis.smu.edu.sg>
2023-10-23 15:02:41 +0200

    Task 4

--- Report.md
+++ Report.md
@@ -1,7 +1,7 @@
 Assignment 1
 ------------
 
-# Team Members: Joey & Lorena
+# Team Members: Joey Run-Qi Lau & Lorena Raichle
 
 # GitHub link to your (forked) repository
 https://github.com/LorenaRaichle/Assignment2.git
@@ -11,15 +11,13 @@ https://github.com/LorenaRaichle/Assignment2.git
 
 1. Indicate the time necessary for the SimpleCrawler to work.
 
-Ans: duration simple crawler: 153608 (miliseconds)
-
-
+Ans: 153608 (miliseconds)
 
 # Task 2
 
 1. Is the flipped index smaller or larger than the initial index? What does this depend on?
 
-Ans:the flipped index csv file is smaller than the initial index since in the initial index there are duplicates.
+Ans: The flipped index csv file is smaller than the initial index since in the initial index there are duplicates.
 In the flipped index file, each keyword with corresponding urls where the keyword can be found are registered without any duplicates.
 
 # Task 3
@@ -27,19 +25,18 @@ In the flipped index file, each keyword with corresponding urls where the keywor
 1. Explain your design choices for the API design.
 
 Ans:
-- for the main page ("/"), search ("/search") and lucky ("/lucky") endpoints we decided to use the GET method to retrieve information from the server without adding any changes. To retrieve search results and render the found urls, we consider the get method as an appropriate choice.
-- in the ("admin/crawler") endpoint we would define a POST method for crawling the environment. Since an update of the index / flippedIndexFiles on the server is necessary when crawling, the POST method is appropriate.
--  in the ("admin/delete") endpoint a DELETE Method to delete a url from the index is appropriate.
-- in the ("admin/update") endpoint we would use a PUT method to upate already existing url information in the index. 
-- in the ("admin/regenerate") endpoint we would define a PATCH method to regenerate the flippedIndexFile because only partial updates are made to a already existing resource.  
+- For the main page ("/"), search ("/search") and lucky ("/lucky") endpoints, we decided to use the GET method to retrieve information from the server without adding any changes. To retrieve search results and render the found urls, we consider the get method as an appropriate choice.
+- In the ("admin/crawler") endpoint, we would define a POST method for crawling the environment. Since an update of the index / flippedIndexFiles on the server is necessary when crawling, the POST method is appropriate.
+-  In the ("admin/delete") endpoint, a DELETE Method to delete a url from the index is appropriate.
+- In the ("admin/update") endpoint, we would use a PUT method to update an existing url information in the index. 
+- In the ("admin/regenerate") endpoint, we would define a PATCH method to regenerate the flippedIndexFile because only partial updates are made to an existing resource. 
+
 # Task 4
 
 1.  Indicate the time necessary for the MultithreadCrawler to work.
-    / to do
-    Ans:
 
-3. Indicate the ratio of the time for the SimpleCrawler divided by the time for the MultithreadedCrawler to get the increase in speed.
-   / to do
-   Ans:
+Ans: 6045 (miliseconds)
 
+2. Indicate the ratio of the time for the SimpleCrawler divided by the time for the MultithreadedCrawler to get the increase in speed.
 
+Ans: 25.4 
--- src/main/java/com/example/searchengine/MultithreadCrawler.java
+++ src/main/java/com/example/searchengine/MultithreadCrawler.java
@@ -1,25 +1,24 @@
 package com.example.searchengine;
 
-import com.opencsv.CSVWriter;
-import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import com.opencsv.CSVWriter;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
 import java.util.concurrent.*;
 
 public class MultithreadCrawler extends Crawler {
 
     private ThreadPoolTaskExecutor executorService;
 
-
-
     private CopyOnWriteArraySet<String> visited;
 
     private CopyOnWriteArraySet<String[]> lines;
-
-
 
     private ObserveRunnable observeRunnable;
 
@@ -28,17 +27,45 @@ public class MultithreadCrawler extends Crawler {
     public MultithreadCrawler(String indexFileName){
         //TODO: initialize
         super(indexFileName);
+
+        int availableCores = Runtime.getRuntime().availableProcessors();
+        executorService = new ThreadPoolTaskExecutor();
+        executorService.setCorePoolSize(availableCores);
+        executorService.initialize();
+
+        visited = new CopyOnWriteArraySet<>();
+        lines = new CopyOnWriteArraySet<>();
+
+        /* 
+            Needs to be initialised before observerThread to ensure that observeRunnable is properly associated 
+            with the observerThread and can monitor the executorService correctly. 
+        */
+        observeRunnable = new ObserveRunnable(this);
+
+        this.crawl("https://api.interactions.ics.unisg.ch/hypermedia-environment/cc2247b79ac48af0");
+
     }
 
     public void crawl(String startUrl){
         double startTime = System.currentTimeMillis();
+        System.out.println(startTime);
         //TODO: complete
+        executorService.submit(new CrawlerRunnable(this, startUrl));
+        Thread observerThread = new Thread(observeRunnable);
+        observerThread.start();
+
+        // Wait for observerThread to complete
+        try {
+            observerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+
         double endTime = System.currentTimeMillis();
+        System.out.println(endTime);
         double duration = endTime - startTime;
-        System.out.println("duration: "+duration);
-
+        System.out.println("duration: " + duration);
     }
-
 
     /*
       TODO: complete class.
@@ -46,7 +73,7 @@ public class MultithreadCrawler extends Crawler {
       1. Process the page at the given url (startUrl).
       2. Create new jobs for the hyperlinks found in the page.
       The instances of this class are used as input to the executorService.submit method.
-       */
+    */
     class CrawlerRunnable implements Runnable{
 
         MultithreadCrawler crawler;
@@ -56,20 +83,59 @@ public class MultithreadCrawler extends Crawler {
         public CrawlerRunnable(MultithreadCrawler crawler, String startUrl){
             this.crawler = crawler;
             this.startUrl = startUrl;
-
         }
 
         @Override
         public void run() {
 
+            String[] line = new String[4];
+            
+            try {
+                // Fetch and parse the webpage at startUrl
+                Document document = Jsoup.connect(startUrl).get();
+
+                // Extract hyperlinks from the webpage
+                Elements links = document.select("a[href]");
+
+                // Extract words from the webpage
+                Elements paragraph = document.select("p");
+
+                line[0] = "/" + startUrl.split("/")[4];
+
+                for (Element link : links) {
+                    String href = link.attr("abs:href");
+
+                    // Check if the link hasn't been visited already
+                    if (!crawler.visited.contains(href)) {
+                        // Mark the link as visited
+                        crawler.visited.add(href);
+
+                        // Create a new crawl job for the link
+                        crawler.executorService.submit(new CrawlerRunnable(crawler, href));
+                    }
+                }
+
+                int count = 1;
+                // Process the webpage (extract data and update lines) 
+                for (Element p : paragraph) {
+                    line[count] = p.text().trim();
+                    count++;
+                }
+
+                crawler.lines.add(line);
+                System.out.println(line);
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
     /*TODO: complete class.
-    The purpose of this class is to monitor whether the executorService has completed all its threads and then print
-    the lines on the index.csv file.
-    This runnable should be run on a thread separate from the executorService.
-
+        The purpose of this class is to monitor whether the executorService has completed all its threads and then print
+        the lines on the index.csv file.
+        This runnable should be run on a thread separate from the executorService.
      */
     class ObserveRunnable implements Runnable {
         private MultithreadCrawler crawler;
@@ -82,6 +148,36 @@ public class MultithreadCrawler extends Crawler {
         @Override
         public void run() {
             //TODO: complete
+
+            System.out.println("Observer thread started.");
+
+            while (true) {
+                if (executorService.getActiveCount() == 0 && executorService.getQueueSize() == 0) {
+                    try {
+
+                        System.out.println("Observer thread completed.");
+
+                        FileWriter fileWriter = new FileWriter(indexFileName);
+                        CSVWriter writer = new CSVWriter(fileWriter, ',', CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.NO_ESCAPE_CHARACTER, "\r\n"); //TODO: macOS and Linux users should change Line to "\n".
+
+                        for (String[] line : lines) {
+                            writer.writeNext(line);
+                        }
+
+                        writer.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                } else {
+                    try {
+                        // Sleep and check again
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     }
 }
